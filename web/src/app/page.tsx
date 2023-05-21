d7befc710768c47289b34f6b99145871ab34ecf1
Rafael Souza <rpsouza.dev@gmail.com>
2023-05-21 08:51:48 -0300

    Add user profile, hero and copyright section

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,7 @@
+import { User } from 'lucide-react'
+import nlwLogo from '../assets/nlw-spacetime-logo.svg'
+import Image from 'next/image'
+
 export default function Home() {
   return (
     <main className="grid min-h-screen grid-cols-2">
@@ -8,6 +12,56 @@ export default function Home() {
 
         {/* Stripes */}
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+
+        {/* Sing-in */}
+        <a
+          href=""
+          className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
+        >
+          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
+            <User className="h-5 w-5 text-gray-500" />
+          </div>
+
+          <p className="max-w-[8.75rem] text-sm leading-snug">
+            <span className="underline">Crie sua conta</span> e salve suas
+            memórias!
+          </p>
+        </a>
+
+        {/* Hero */}
+        <div className="space-y-5">
+          <Image src={nlwLogo} alt="NLW Spacetime" />
+
+          <div className="max-w-[26.25rem] space-y-1">
+            <h1 className="text-5xl font-bold leading-tight text-gray-50">
+              Sua cápsula do tempo
+            </h1>
+            <p className="text-lg leading-relaxed">
+              Colecione momentos marcantes da sua jornada e compartilhe (se
+              quiser) com o mundo!
+            </p>
+          </div>
+
+          <a
+            className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black transition-colors hover:bg-green-600"
+            href=""
+          >
+            cadastrar lembrança
+          </a>
+        </div>
+
+        {/* Copyright */}
+        <div className="text-sm leading-relaxed text-gray-200">
+          Feito com 💜 no NLW da{' '}
+          <a
+            className="underline transition-colors hover:text-gray-100"
+            href="https://rocketseat.com.br"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Rocketseat
+          </a>
+        </div>
       </div>
 
       {/* Right */}
